16d1586292af732dd2026db1d4b3aad7896f96d2
Ryoma <Ryoma3540@gmail.com>
2024-06-14 14:36:54 -0700

    dev google form

--- app/components/layouts/Contact.tsx
+++ app/components/layouts/Contact.tsx
@@ -8,13 +8,14 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { useToast } from "@/components/ui/use-toast"
+
 
 const formSchema = z.object({
   name: z.string().min(2).max(50),
@@ -22,8 +23,11 @@ const formSchema = z.object({
   content: z.string().min(2).max(500),
 });
 
+const googleFormUrl = process.env.NEXT_PUBLIC_GOOGLE_FORM_URL;
+
 const Contact = () => {
-  // 1. Define your form.
+  const { toast } = useToast()
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -33,19 +37,44 @@ const Contact = () => {
     },
   });
 
-  // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+    const formData = new FormData();
+    formData.append(`entry.${process.env.NEXT_PUBLIC_NAME_ENTRY_ID}`, values.name);
+    formData.append(`entry.${process.env.NEXT_PUBLIC_EMAIL_ENTRY_ID}`, values.email);
+    formData.append(`entry.${process.env.NEXT_PUBLIC_CONTENT_ENTRY_ID}`, values.content);
+
+    if (!googleFormUrl) {
+      console.error("Google Form URL is not defined.");
+      return;
+    }
+
+    fetch(googleFormUrl, {
+      method: "POST",
+      body: formData,
+      mode: "no-cors",
+    }).then(() => {
+      toast({
+        title: "✅ Success!!",
+        description: "Thank you for your message!!",
+      })
+      form.reset();
+    }).catch((error) => {
+      toast({
+        title: "❌ Failed!!",
+        description: error,
+      })
+      form.reset();
+    });
   }
+
+
   return (
     <section className="px-10 py-14 md:py-20 xl:py-32" id="Contact">
       <div className="xl:w-[1000px] xl:mx-auto">
         <Title title="Contact" />
         <div className="mt-10 sm:w-4/5 sm:mx-auto md:w-3/5 xl:mt-16">
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8" action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSfRQ-NPSU8nrbKrpC45qWqeFdgsPzmxqIbk7jTc9WRH2G1MyQ/formResponse">
               {/* name */}
               <FormField
                 control={form.control}
